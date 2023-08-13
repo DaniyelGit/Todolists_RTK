e2a6d29940fd39b4ef4f1abb72413533a1dac9bd
Daniyel Tkachuk <daniyel_github-1@mail.ru>
2023-08-13 20:02:11 +0300

    refactor code

--- src/common/utils/handle-server-network-error.ts
+++ src/common/utils/handle-server-network-error.ts
@@ -1,6 +1,13 @@
 import { Dispatch } from "redux";
-import { AxiosError, isAxiosError } from "axios";
+import { isAxiosError } from "axios";
 import { appActions } from "app/app-reducer";
+
+/**
+ * Обрабатывает ошибки сети, возникающие при отправке запросов на сервер
+ * @param {unknown} err - Ошибка, которая произошла при отправке запроса на сервер
+ * @param {Dispatch} dispatch - Функция dispatch из библиотеки Redux для отправки actions
+ * @returns {void} - Данная функция ничего не возвращает
+ */
 
 export const handleServerNetworkError = (err: unknown, dispatch: Dispatch) => {
    let errorMessage = "Some error occurred";
